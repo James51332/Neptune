946e11472dc6733e9f9e2c5dd454930249037a2f
James51332 <jamesddadabo@gmail.com>
2021-08-13 22:47:38 -0500

    Created Premake File and Test Files

---
+++ premake5.lua
@@ -0,0 +1,89 @@
+workspace "Neptune"
+	location "build"
+	language "C++"
+	cppdialect "C++17"
+	architecture "x86_64"
+	startproject "Sandbox"	
+	staticruntime "On"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+project "Neptune"
+	kind "StaticLib"
+	location "build"
+
+	targetdir ("build/bin/neptune-" .. string.lower("%{cfg.system}") .. "/%{cfg.shortname}/")
+	objdir ("build/bin-obj/neptune-" .. string.lower("%{cfg.system}") .. "/%{cfg.shortname}/")
+
+	files
+	{
+		"src/**.cpp",
+		"src/**.h"
+	}
+
+	filter "system:macosx"
+		defines "NEPTUNE_MACOS"
+
+	filter "system:windows"
+		defines "NEPTUNE_WINDOWS"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "NEPTUNE_DEBUG"
+		symbols "On"		
+
+	filter "configurations:Release"
+		defines "NEPTUNE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NEPTUNE_DIST"
+		optimize "Full"
+
+project "Sandbox"
+	kind "ConsoleApp"
+	
+	debugdir "."
+
+	targetdir ("build/bin/neptune-" .. string.lower("%{cfg.system}") .. "/%{cfg.shortname}/")
+        objdir ("build/bin-obj/neptune-" .. string.lower("%{cfg.system}") .. "/%{cfg.shortname}/")
+
+	files
+	{
+		"sandbox/**.cpp",
+		"sandbox/**.h"
+	} 
+
+	links
+	{
+		"Neptune"
+	}
+
+	filter "system:macosx"
+                defines "NEPTUNE_MACOS"
+
+        filter "system:windows"
+                defines "NEPTUNE_WINDOWS"
+                systemversion "latest"
+
+        filter "configurations:Debug"
+                defines "NEPTUNE_DEBUG"
+		symbols "On"
+
+        filter "configurations:Release"
+                defines "NEPTUNE_RELEASE"
+		optimize "On"
+	
+        filter "configurations:Dist"
+                defines "NEPTUNE_DIST"
+		optimize "Full"
